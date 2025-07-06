6e4e6c2b529c08f91ad339db586eff9e7b9a6bfa
CinnamonBun69 <Real.godl664@gmail.com>
2025-07-06 15:15:58 +0300

    hw_23: migrations done, models registered

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Master, Order, Review, Service
 
 # Register your models here.
+admin.site.register(Master)
+admin.site.register(Order)
+admin.site.register(Review)
+admin.site.register(Service)
