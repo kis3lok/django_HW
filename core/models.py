a62a66eb9505ff767d4d644056e4c3dbecf4612c
CinnamonBun69 <Real.godl664@gmail.com>
2025-07-06 15:14:02 +0300

    hw_23: created Service model

--- core/models.py
+++ core/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Service(models.Model):
+    """
+    Модель услуги барбершопа
+    """
+    name = models.CharField(max_length=200, verbose_name="Название")
+    description = models.TextField(blank=True, verbose_name="Описание")
+    price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="Цена")
+    duration = models.PositiveIntegerField(verbose_name="Длительность", help_text="Время выполнения в минутах")
+    is_popular = models.BooleanField(default=False, verbose_name="Популярная услуга")
+    image = models.ImageField(upload_to="services/", blank=True, verbose_name="Изображение")
+
+    class Meta:
+        verbose_name = "Услуга"
+        verbose_name_plural = "Услуги"
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
+
