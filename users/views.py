ded95eed12bb8cdb5e5581ca95a3c77660fc6d8b
CinnamonBun69 <Real.godl664@gmail.com>
2025-08-09 00:50:10 +0300

    hw_30: views updated

--- users/views.py
+++ users/views.py
@@ -1,29 +1,58 @@
-from django.shortcuts import render, redirect
-from django.contrib.auth import login, logout
+from django.views.generic import CreateView
+from django.contrib.auth.views import LoginView, LogoutView
+from django.urls import reverse_lazy
+from django.contrib import messages
+from django.contrib.auth import login
 from .forms import UserRegisterForm, UserLoginForm
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import TemplateView
 
-def register_view(request):
-    if request.method == "POST":
-        form = UserRegisterForm(request.POST)
-        if form.is_valid():
-            user = form.save()
-            login(request, user)
-            return redirect("login")
-    else:
-        form = UserRegisterForm()
-    return render(request, "users/register.html", {"form": form})
+class UserRegisterView(CreateView):
+    form_class = UserRegisterForm
+    template_name = 'users/register.html'
+    success_url = reverse_lazy('login')
 
-def login_view(request):
-    if request.method == "POST":
-        form = UserLoginForm(request, data=request.POST)
-        if form.is_valid():
-            user = form.get_user()
-            login(request, user)
-            return redirect("landing")
-    else:
-        form = UserLoginForm()
-    return render(request, "users/login.html", {"form": form})
+    def form_valid(self, form):
+        response = super().form_valid(form)
+        user = form.save()
+        login(self.request, user)
+        messages.success(self.request, 'Вы успешно зарегистрировались!')
+        return response
 
-def logout_view(request):
-    logout(request)
-    return redirect("landing")
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Регистрация'
+        return context
+
+class UserLoginView(LoginView):
+    form_class = UserLoginForm
+    template_name = 'users/login.html'
+    redirect_authenticated_user = True
+
+    def get_success_url(self):
+        return reverse_lazy('landing')
+
+    def form_invalid(self, form):
+        messages.error(self.request, 'Неверное имя пользователя или пароль')
+        return super().form_invalid(form)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Вход'
+        return context
+
+class UserLogoutView(LogoutView):
+    next_page = reverse_lazy('landing')
+
+    def dispatch(self, request, *args, **kwargs):
+        messages.info(request, 'Вы успешно вышли из системы')
+        return super().dispatch(request, *args, **kwargs)
+    
+class ProfileView(LoginRequiredMixin, TemplateView):
+    template_name = 'users/profile.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Личный кабинет'
+        # Здесь можно добавить дополнительные данные пользователя
+        return context
