d598c4f2c504f8fee9d9058afece1070ff399599
CinnamonBun69 <Real.godl664@gmail.com>
2025-08-09 00:51:17 +0300

    hw_30: urls updated, work on password_change started

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,21 @@
-from django.urls import path
-from . import views
+from django.urls import path, reverse_lazy
+from .views import UserRegisterView, UserLoginView, UserLogoutView, ProfileView
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    path('register/', views.register_view, name='register'),
-    path('login/', views.login_view, name='login'),
-    path('logout/', views.logout_view, name='logout'),
+    path('register/', UserRegisterView.as_view(), name='register'),
+    path('login/', UserLoginView.as_view(), name='login'),
+    path('logout/', UserLogoutView.as_view(), name='logout'),
+    path('profile/', ProfileView.as_view(), name='profile'),
+    path('password_change/', 
+         auth_views.PasswordChangeView.as_view(
+             template_name='users/password_change_form.html',
+             success_url=reverse_lazy('password_change_done')
+         ), 
+         name='password_change'),
+    path('password_change/done/', 
+         auth_views.PasswordChangeDoneView.as_view(
+             template_name='users/password_change_done.html'
+         ), 
+         name='password_change_done'),
 ]
